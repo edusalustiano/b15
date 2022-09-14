9c0589ef84860b46e7b4519e00b06312f7d870ca
edu salustiano <edusalustiano5@gmail.com>
2022-09-14 13:44:51 -0300

    corrigir bug

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function mostra2(){
 }
 function bta(){
 
-     document.getElementById("demo").src="https://video-private-assets.canva.com/VAFMG2V94IQ/v/91c3df4ddd.mp4?exp=1663164960000&cf-ck=i7-_UZjIvhQb8TDPZQmwA1Mqhnakrw20Q4Q3PalCLnU&cf-sig=oWdYqPdqyOisYbtTawRtFZHdicZ4AthZwVUHQ7PM1kA&cf-sig-kid=CO7cCjZ_YiI=&sig=uZHp7ddFyraiNY7KvqHXcHWbaWJFbauD4TErQdj3tb8&sig-kid=GzFgFdhXD-Q=";
+     document.getElementById("demo").src="https://video-private-assets.canva.com/VAFMG2V94IQ/v/91c3df4ddd.mp4?exp=1663215360000&cf-ck=i7-_UZjIvhQb8TDPZQmwA1Mqhnakrw20Q4Q3PalCLnU&cf-sig=fLBkyexWtZfxhyR-YRbHivjYMo_IGEX5il5ZBUX-lc8&cf-sig-kid=CO7cCjZ_YiI=&sig=8KLbsTErnltSQG9AOJiL4fY-2waR0MCBatHxEZfUJqM&sig-kid=GzFgFdhXD-Q=";
      document.getElementById("filmes").style.display = "none";
      document.getElementById('cinema').style.display = "block";
 
@@ -29,30 +29,30 @@ function bta(){
     
 
 function bte(){
-    document.getElementById("demo").src="https://video-private-assets.canva.com/VAFMHfawLBg/v/4b92221a5e.mp4?exp=1663165440000&cf-ck=Behzlu9KlQABVQkU0exaB6FVt8M72BUvoCZ8SBVWUFE&cf-sig=Owab-02a8lxsMcWF0CH9KASGtnGN598N4Ri7-M9g5hk&cf-sig-kid=CO7cCjZ_YiI=&sig=nlzBYinvaUpDE340usdEUll3FtkHku0Z1NuRsweRtXI&sig-kid=GzFgFdhXD-Q=";
+    document.getElementById("demo").src="https://video-private-assets.canva.com/VAFMHfawLBg/v/4b92221a5e.mp4?exp=1663215840000&cf-ck=Behzlu9KlQABVQkU0exaB6FVt8M72BUvoCZ8SBVWUFE&cf-sig=wTWd96_GeJiuLxQeE1BAzi7ImKdfmOBOJE0ln5FzBdA&cf-sig-kid=CO7cCjZ_YiI=&sig=XlqUuHwi2fE9H29jMa-r_-jTV74_J9wXwd5cOmBdiHM&sig-kid=GzFgFdhXD-Q=";
     document.getElementById("filmes").style.display = "none";
      document.getElementById('cinema').style.display = "block";
 }
 
 function bti(){
-    document.getElementById("demo").src="https://video-private-assets.canva.com/VAFMH6Tp4yo/v/225cb514da.mp4?exp=1663134060000&cf-ck=2esa_Bh-aZu6pNJaNygXQ3VuWXSuSl_-58GPun72QQE&cf-sig=Q3cCPaaLTOUUM1wjlnVVrN4i6jwCG_gmuK0SQz0r4To&cf-sig-kid=CO7cCjZ_YiI=&sig=VF9wbiFh7zDPESMBNo4M2hAa4g9mrH3bIJx10pevKlg&sig-kid=GzFgFdhXD-Q=";
+    document.getElementById("demo").src="https://video-private-assets.canva.com/VAFMH6Tp4yo/v/225cb514da.mp4?exp=1663220460000&cf-ck=2esa_Bh-aZu6pNJaNygXQ3VuWXSuSl_-58GPun72QQE&cf-sig=UnbT8hRMr65jO5AeQQqhPGQB9plPcW48jkyIfIRQ2U8&cf-sig-kid=CO7cCjZ_YiI=&sig=Hz2LOExoVW28EaY1uU4phONtJBsI_mfsE_HNwjV1RXE&sig-kid=GzFgFdhXD-Q=";
     document.getElementById("filmes").style.display = "none";
      document.getElementById('cinema').style.display = "block";
 }
 function bto(){
-    document.getElementById("demo").src="https://video-private-assets.canva.com/VAFMIHhnfXI/v/ae210044cb.mp4?exp=1663131960000&cf-ck=WJIdZodliWljDw0fl0VEqKwFBUGLWNmaB6mfe0OqU_M&cf-sig=ywcgZh4tWSazJa53JGKCUlcogLk20WwIz1a0QdZ4BRU&cf-sig-kid=CO7cCjZ_YiI=&sig=K9BiCiA6nOwVi_84lqHipYi14r7bZNGxI_n-IZX_Xic&sig-kid=GzFgFdhXD-Q=" ;  
+    document.getElementById("demo").src="https://video-private-assets.canva.com/VAFMIHhnfXI/v/ae210044cb.mp4?exp=1663214760000&cf-ck=WJIdZodliWljDw0fl0VEqKwFBUGLWNmaB6mfe0OqU_M&cf-sig=uE4kPh6YKaNQqh1D5g-1w1GrxyxBhhsM4slP363121A&cf-sig-kid=CO7cCjZ_YiI=&sig=IEUj2OaYZSoeIQvzZzTy6juaqZDml-KV7ix25JXDFnQ&sig-kid=GzFgFdhXD-Q=" ;  
     document.getElementById("filmes").style.display = "none";
      document.getElementById('cinema').style.display = "block";
 }
 
 function btu(){
-    document.getElementById("demo").src="https://video-private-assets.canva.com/VAFMIe4vZU8/v/287b1c05e0.mp4?exp=1663137480000&cf-ck=Yj2Oz8L05JyH7r4Pptz_N3sEh6sthyG65HlaFiVpICc&cf-sig=2sYM500R2bVBkaE5iUIj_q5rrNJak_hhpKLGyyzXlCk&cf-sig-kid=CO7cCjZ_YiI=&sig=rO1KOBMe-9k3vXfeKEPl1aR4nVgKOOfgRVzinjRhI0M&sig-kid=GzFgFdhXD-Q=" ;
+    document.getElementById("demo").src="https://video-private-assets.canva.com/VAFMIe4vZU8/v/287b1c05e0.mp4?exp=1663216680000&cf-ck=Yj2Oz8L05JyH7r4Pptz_N3sEh6sthyG65HlaFiVpICc&cf-sig=31oEoeum34itDQBw-aXMeszEI0JkmmKUBJHgOkRkW5o&cf-sig-kid=CO7cCjZ_YiI=&sig=OQJOSCPFu9yQOhSSy-4_rMaUNuaPsgqqjFO8M1gdOYQ&sig-kid=GzFgFdhXD-Q=" ;
     document.getElementById("filmes").style.display = "none";
      document.getElementById('cinema').style.display = "block";
 }
 
 function btb(){
-    document.getElementById("demo").src="videos/verções de um crime.mp4";
+    document.getElementById("demo").src="https://video-private-assets.canva.com/VAFMJRESfwI/v/4f628b2223.mp4?exp=1663221120000&cf-ck=HpIRqjv0JfQHFm3DLXAclA0hMr-2bgdUS8fxsOqpMWg&cf-sig=_i-9aoNuaswPlPThW9hyyrMpMlPIDhkiNvTLryShFD8&cf-sig-kid=CO7cCjZ_YiI=&sig=xeSG1n6Kz17CwhNsfMaCnZG-UOnikGpIx3tlyg6kXbM&sig-kid=GzFgFdhXD-Q=";
     document.getElementById("filmes").style.display = "none";
      document.getElementById('cinema').style.display = "block";
 }
@@ -64,13 +64,13 @@ function btc(){
 }
 
 function btd(){
-    document.getElementById("demo").src="videos/Revolta.mp4";
+    document.getElementById("demo").src="https://video-private-assets.canva.com/VAFMJRESfwI/v/4f628b2223.mp4?exp=1663221120000&cf-ck=HpIRqjv0JfQHFm3DLXAclA0hMr-2bgdUS8fxsOqpMWg&cf-sig=_i-9aoNuaswPlPThW9hyyrMpMlPIDhkiNvTLryShFD8&cf-sig-kid=CO7cCjZ_YiI=&sig=xeSG1n6Kz17CwhNsfMaCnZG-UOnikGpIx3tlyg6kXbM&sig-kid=GzFgFdhXD-Q=";
     document.getElementById("filmes").style.display = "none";
      document.getElementById('cinema').style.display = "block";
 }
 
 function bt1(){
-    document.getElementById("demo").src="https://video-private-assets.canva.com/VAFMHB6mbEg/v/1f27dc08bf.mp4?exp=1663165380000&cf-ck=Pg5wuf_wPd5y3Hx13ocAQqwVZD0NCf4-dR_RlKFmIto&cf-sig=Oz91SgZmEFDxy8zgyjfdj4006Ke-K-5kpaLi7vgbs00&cf-sig-kid=CO7cCjZ_YiI=&sig=giX-vN_PMutVRCGdySAzcB8x1rw9yW-5FBAZYFBz288&sig-kid=GzFgFdhXD-Q=";
+    document.getElementById("demo").src="https://video-private-assets.canva.com/VAFMHB6mbEg/v/1f27dc08bf.mp4?exp=1663219380000&cf-ck=Pg5wuf_wPd5y3Hx13ocAQqwVZD0NCf4-dR_RlKFmIto&cf-sig=Ha4Lof5SfgZxJWhhFuIdZYiVoaocKeyYynVL1ZrO6XQ&cf-sig-kid=CO7cCjZ_YiI=&sig=d7WEHBuE-wlaeWJdOR2QI-Fe_KFQ4r9pstS-1LTCNqg&sig-kid=GzFgFdhXD-Q=";
     document.getElementById("filmes").style.display = "none";
      document.getElementById('cinema').style.display = "block";
 
@@ -78,7 +78,7 @@ function bt1(){
 
 
 function bt2(){
-    document.getElementById("demo").src="https://video-private-assets.canva.com/VAFMHTsuPzc/v/bc34a37fb7.mp4?exp=1663121280000&cf-ck=sB8i1LvoVu9OdXYVhfqtW7IdPDIRSTkMFCmQGJrPG-0&cf-sig=tEnVu5LXJWdZRKq_jtkWbk0QajWY27Wcw7Wn--n5qlU&cf-sig-kid=CO7cCjZ_YiI=&sig=6SA6ARpZeHpwskktWVQVh5esPlHcy21VGqKcwMEFZIo&sig-kid=GzFgFdhXD-Q=";
+    document.getElementById("demo").src="https://video-private-assets.canva.com/VAFMHTsuPzc/v/bc34a37fb7.mp4?exp=1663218480000&cf-ck=sB8i1LvoVu9OdXYVhfqtW7IdPDIRSTkMFCmQGJrPG-0&cf-sig=t1opTKbLKdgPfrCf0AhXF0RHuxwNvIyJAN_m-O_E-SM&cf-sig-kid=CO7cCjZ_YiI=&sig=q1tWQK83OdzsNNTlm-ycFFMyp2IY0uvvypnmtGrfTHE&sig-kid=GzFgFdhXD-Q=";
     document.getElementById("filmes").style.display = "none";
      document.getElementById('cinema').style.display = "block";
 }
